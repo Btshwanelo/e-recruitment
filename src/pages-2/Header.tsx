8c023edce331eae56e80975c06770cc3d88a5495
Tshwanelo Bucibo <tshwanelo@spacce.io>
2025-05-13 13:56:00 +0200

    Update Header.tsx

--- src/pages-2/Header.tsx
+++ src/pages-2/Header.tsx
@@ -8,7 +8,7 @@ const HeaderV2 = () => {
         <div className="container mx-auto px-4 flex items-center justify-between">
           <div className="flex items-center">
             <div className="w-20 h-14 rounded flex items-center justify-center">
-              <img src="pic-logo-small.png" alt="EZRA Logo" className="w-24 h-auto" />
+              <img src="/pic-logo-small.png" alt="EZRA Logo" className="w-24 h-auto" />
             </div>
             <nav className="ml-8 hidden md:flex space-x-6">
               {/* <a  onClick={()=>navigate('/applications')} className="text-[#475467] cursor-pointer font-semibold hover:text-blue-500">Home</a> */}
